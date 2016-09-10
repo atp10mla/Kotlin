a79d3498c9f945610fc9467ec3be2e969bef122e
Markus <karl.markus.larsson@gmail.com>
2016-09-10 15:09:06 +0200

    add google play services

--- app/src/main/java/larsson/silver/kotlin/view/activities/BarcodeScannerActivity.kt
+++ app/src/main/java/larsson/silver/kotlin/view/activities/BarcodeScannerActivity.kt
@@ -12,6 +12,6 @@ class BarcodeScannerActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_barcode_scanner)
     }
 }
