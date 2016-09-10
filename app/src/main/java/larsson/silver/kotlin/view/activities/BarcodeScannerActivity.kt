1f6a973401169c0e15614ff37053b81b81e17e39
Markus <karl.markus.larsson@gmail.com>
2016-09-10 15:20:11 +0200

    update manifest

--- app/src/main/java/larsson/silver/kotlin/view/activities/BarcodeScannerActivity.kt
+++ app/src/main/java/larsson/silver/kotlin/view/activities/BarcodeScannerActivity.kt
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import larsson.silver.kotlin.R
 
+
+import com.google.android.gms.vision.barcode.Barcode;
+
 /**
  * Created by ossi on 10/09/16.
  */
@@ -13,5 +16,17 @@ class BarcodeScannerActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_barcode_scanner)
+    }
+
+    fun initScanner() {
+
+        /*
+        val barcodeDetector = BarcodeDetector.Builder(context).build()
+        val barcodeFactory = BarcodeTrackerFactory()
+        barcodeDetector.setProcessor(
+                MultiProcessor.Builder(barcodeFactory).build())
+
+        mCameraSource = CameraSource.Builder(context, barcodeDetector).setFacing(CameraSource.CAMERA_FACING_BACK).setRequestedPreviewSize(1600, 1024).build()
+        */
     }
 }
