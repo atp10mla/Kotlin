56f35e036fa9c060b747f6bf26c7be712ca20a67
Markus <karl.markus.larsson@gmail.com>
2016-09-10 12:18:51 +0200

    KOTLIN!

---
+++ app/src/main/java/larsson/silver/kotlin/MainActivity.kt
@@ -0,0 +1,16 @@
+package larsson.silver.kotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by markus on 16-09-10.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
