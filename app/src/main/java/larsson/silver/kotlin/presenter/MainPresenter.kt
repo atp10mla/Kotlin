bcb2efa73d062ce298b85301ae26ea5ffce0a862
Markus <karl.markus.larsson@gmail.com>
2016-09-10 13:28:46 +0200

    update structure

---
+++ app/src/main/java/larsson/silver/kotlin/presenter/MainPresenter.kt
@@ -0,0 +1,14 @@
+package larsson.silver.kotlin.presenter
+
+/**
+ * Created by markus on 16-09-10.
+ */
+
+
+class MainPresenter {
+    fun onCreate() {
+
+
+    }
+
+}
--- app/src/main/java/larsson/silver/kotlin/view/activities/MainActivity.kt
+++ app/src/main/java/larsson/silver/kotlin/view/activities/MainActivity.kt
@@ -1,16 +1,26 @@
-package larsson.silver.kotlin
+package larsson.silver.kotlin.view.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import larsson.silver.kotlin.R
+import larsson.silver.kotlin.presenter.MainPresenter
 
 /**
  * Created by markus on 16-09-10.
  */
 class MainActivity : AppCompatActivity() {
 
+    val mMainPresenter: MainPresenter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 
+
+
 }
+
